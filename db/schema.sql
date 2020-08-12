8762dde498845cdd7f37b39b466c7ab25de108d2
lafry5 <lafry5@yahoo.com>
2020-08-12 11:58:05 -0700

    verified initial seed input in seeds.sql file

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INTEGER,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR (30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee
+(first_name, last_name)
+VALUES
+('John', 'Doe');
